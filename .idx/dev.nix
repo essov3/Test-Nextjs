44c76703501ede928acb48ed8c7622ef04d675fb
eslamr123 <eslamr123@gmail.com>
2024-06-02 18:15:11 +0000

    init

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,17 +1,30 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
-{pkgs}: {
+
+{ pkgs }: {
+  services.docker.enable = false;
+
   # Which nixpkgs channel to use.
   channel = "stable-23.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
+
+    
     pkgs.nodejs_20
     pkgs.yarn
     pkgs.nodePackages.pnpm
     pkgs.bun
+
+
+
+
+
+
   ];
   # Sets environment variables in the workspace
-  env = {};
+  env = {
+ 
+   };
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
@@ -23,13 +36,18 @@
         npm-install = "npm install";
       };
       # To run something each time the workspace is (re)started, use the `onStart` hook
+      onStart = {
+
+      };
+
     };
     # Enable previews and customize configuration
     previews = {
       enable = true;
       previews = {
+   
         web = {
-          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
+          command = [ "npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0" ];
           manager = "web";
         };
       };
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,9 +3,9 @@ export default function LoginPage() {
         <div className="flex flex-col items-center justify-center h-screen ">
             <h1>Login</h1>
 
-            <form method="post" action="/api/login">
-                <input type="email" name="email" placeholder="Email" required />
-                <input type="password" name="password" placeholder="Password" required />
+            <form method="post" action="/api/login" className="flex flex-col items-center justify-center bg-gray-800 m-6 p-4 rounded ">
+                <input type="email" name="email" placeholder="Email" required className="m-2" />
+                <input type="password" name="password" placeholder="Password" required className="m-2 bg-yellow-500" />
                 <button type="submit">Login</button>
             </form>
 
